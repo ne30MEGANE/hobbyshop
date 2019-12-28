cc8fbc59f4a1d6ec921a3d965acfcea388d7644e
Takahashi <ne300229@senshu-u.jp>
2019-12-28 17:34:52 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "items#index"
+  resources :items do
+    get "search", on: :collection
+  end
 end
