c1eca28e2a6abcecb9aba11fe5063bec414f503a
Takahashi <ne300229@senshu-u.jp>
2020-01-06 16:45:50 +0900

    login/logout routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "items#index"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
   resources :items do
     get "search", on: :collection
   end
