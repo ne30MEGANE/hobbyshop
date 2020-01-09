423b2efd7b786530c7a2517673bc7f3215b716fa
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-09 19:43:03 +0900

    add seeddata

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_071007) do
+ActiveRecord::Schema.define(version: 2020_01_09_081234) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "admin_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "details", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
+    t.integer "quantitiy", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_details_on_item_id"
+    t.index ["order_id"], name: "index_details_on_order_id"
+  end
 
   create_table "items", force: :cascade do |t|
-    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.string "item_name", null: false
+    t.text "item_details", null: false
     t.integer "stock", null: false
     t.integer "price", null: false
     t.date "releace", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "time", null: false
+    t.integer "pay", null: false
+    t.boolean "delivery", default: false, null: false
+    t.boolean "cancel", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.integer "score"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name", null: false
+    t.string "nickname"
+    t.integer "sex", default: 1, null: false
+    t.string "phone"
+    t.string "email"
+    t.date "birthday", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
