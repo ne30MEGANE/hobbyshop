8e0447fe7ed4f273f70766ef0442eb8e98463e60
Takahashi <ne300229@senshu-u.jp>
2020-01-09 14:30:23 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,24 @@ Rails.application.routes.draw do
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
 
-  resources :items do
+  resources :items, only: [:index, :show] do
     get "search", on: :collection
+    resources :reviews, only; [:index]
+  end
+  resources :reviews
+  resources :users
+  resources :orders
+
+  resource :session, only: [:new, :create, :destroy]
+  resource :account, only: [:edit, :update]
+  resource :cart, only: [:index, :update] do
+    delete "delete", on: :collection
+  end
+
+  namespace :admin do
+    root "items#index"
+    resource :items do
+      get "search", on: :collection
+    end
   end
 end
