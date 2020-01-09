356d8d66e763b5db62417a4a726da27a7da5cbe9
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-09 17:07:23 +0900

    create reviewModel&Controller

---
+++ db/migrate/20200109075423_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.integer :score
+      t.text :text
+      t.timestamps
+    end
+  end
+end
