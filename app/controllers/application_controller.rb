c6b0ab6fa719ac1d92b46907319942c12880acb9
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-10 17:22:18 +0900

    bug fix login&logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
-    def menu_link_to(text, path, options = {})
-        content_tag :li do
-            link_to_unless_current(text, path, options) do
-                content_tag(:span, text)
-            end
-        end
-    end
-    
     private def signed_in? #ログインされてるかどうか
         User.find_by(email: session[:user_id]) if session[:user_id]
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
 		title = @page_title + " | " + title if @pafe_title
 		title
   end
+
+  def menu_link_to(text, path, options = {})
+	content_tag :li do
+		link_to_unless_current(text, path, options) do
+			content_tag(:span, text)
+		end
+	end
+  end
 end
