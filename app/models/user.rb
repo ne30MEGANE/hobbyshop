c25ed7492899ff475fa1ba60e4cc0b7334249287
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-14 00:32:36 +0900

    ユーザーデータベースにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
 
     has_many :reviews
     has_many :orders
+
+    validates :user_name, presence: true, length: {minimum: 2, maximum: 20}
+    validates :nickname, length: { maximum: 20}
+    validates :phone, presence: true, format: {
+      with: /\A[0-9\(\)\-]*\z/,
+      allow_blank: true,
+      message: :invalid_telephone_number
+    }
+    validates :email, presence: true, email: {allow_blank: true}
+    validates :postcode, presence: true, format: {
+      with: /\A[0-9]{3}-[0-9]{4}\z/,
+      allow_blank: true,
+      message: :invalid_postcode
+    }
 end
