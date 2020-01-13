984e4157ca74c55b0f1551b05957ae6c6f4a445f
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-13 23:54:21 +0900

    商品詳細ページを作成（しようとした）

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,4 +2,8 @@ class Admin::ItemsController < Admin::Base
   def index #トップページ
     @items = Item.order("releace")
   end
+
+  def show #商品詳細
+    @item = Item.find([params[:id]])
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,12 @@ class ItemsController < ApplicationController
   def index #トップページ
     @items = Item.order("releace")
   end
+
+  def show #商品詳細
+    @item = Item.find([params[:id]])
+  end
+
+  def search
+
+  end
 end
