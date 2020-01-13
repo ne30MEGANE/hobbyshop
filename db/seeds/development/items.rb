d0a5afb48094551ce00e79d6368f9ae534638099
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-13 23:52:15 +0900

    シードデータがちゃんと生成されていないバグを解消

--- db/seeds/development/items.rb
+++ db/seeds/development/items.rb
@@ -1,9 +1,9 @@
 categorirs = %w(manuel comic CD/DVD software hardware goods)
 date = %w(2019-11-01 2019-12-01 2020-01-01 2020-02-01)
-0.upto(50) do |i|
+0.upto(49) do |i|
     Item.create(
-        category_id: i % 6,
-        item_name: "TestItem#{i}_#{categorirs[i%6]}", #通し番号_カテゴリ名
+        category_id: (i % 6) + 1,
+        item_name: "TestItem#{i+1}_#{categorirs[i%6]}", #通し番号_カテゴリ名
         item_details: "ホビーショップNS限定販売品です！",
         stock: 10,
         price: 300*(i+1),
