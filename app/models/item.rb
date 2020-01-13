63b457110b877f4d45607e2e824d6fefc5722cb8
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-14 00:25:22 +0900

    商品データベースにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
     belongs_to :category
+
+    validates :item_name, presence: true
+    validates :stock, presence: true, 
+      numericality: {
+        only_integer: true,
+        allow_blank: true,
+        greater_than_or_equal_to: 0
+      }
+    validates :price, presence: true,
+      numericality: {
+          only_integer: true,
+          allow_blank: true,
+          greater_than: 0
+      }
 end
