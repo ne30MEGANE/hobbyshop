5a3103502c748d0d91890bc2075463f1f1ffe258
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-14 19:07:44 +0900

    カートに追加する機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
     end
     helper_method :signed_in?
 
+    private def current_cart #@orderを返す
+        if session[:order_id]
+            @order = Order.find(session[:order_id])
+        else
+            @order = Order.create() #新しく作って
+            session[:order_id] = @order.id #セッションにidを保存して
+            @order #orderおｂｊを返す
+        end
+    end
+    helper_method :current_cart
+
     class LoginRequired < StandardError; end
     class Forbidden < StandardError; end
     
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,31 @@
 class OrdersController < ApplicationController
+    before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
+
+    def show
+        @details = current_cart.details
+    end
+
+    def add_item
+        if @detail.blank?
+            @detail = current_cart.details.build(item_id: params[:item_id])
+        end
+        @detail.quantitiy += params[:quantitiy].to_i
+        @detail.save
+        redirect_to Item.find(params[:item_id]), notice: "商品をカートに追加しました"
+    end
+    
+    def update_item
+        @detail.update(quantitiy: params[:quantitiy].to_i)
+        redirect_to current_cart
+    end
+
+    def delete_item
+        @detail.destroy
+        redirect_to current_cart
+    end
+
+    private
+    def setup_cart_item!
+        @detail = current_cart.details.find_by(item_id: params[:item_id])
+    end
 end
