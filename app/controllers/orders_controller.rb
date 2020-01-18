997933d3ded42d7458f09ad172647ee4296269af
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-18 15:31:40 +0900

    カートに投入する個数にバリデーションをつけて、入力欄をnumberタイプに変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,39 @@
 class OrdersController < ApplicationController
     before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
-
+    before_action :remove_errors, only: :add_item
+    
     def show
         @details = current_cart.details.order("item_id")
     end
-
+    
     def add_item
         if @detail.blank?
             @detail = current_cart.details.build(item_id: params[:item_id])
         end
         @detail.quantitiy += params[:quantitiy].to_i
-        @detail.save
-        redirect_to Item.find(params[:item_id]), notice: "商品をカートに追加しました"
+        if @detail.save
+            redirect_to Item.find(params[:item_id]), notice: "商品をカートに追加しました"    
+        else
+            session[:errors] = @detail.errors.full_messages.to_json
+            redirect_to Item.find(params[:item_id])
+        end
     end
     
     def update_item
         @detail.update(quantitiy: params[:quantitiy].to_i)
         redirect_to current_cart
     end
-
+    
     def delete_item
         @detail.destroy
         redirect_to current_cart
     end
-
+    
     private
     def setup_cart_item!
         @detail = current_cart.details.find_by(item_id: params[:item_id])
+    end
+    def remove_errors
+        session.delete(:errors)
     end
 end
--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,4 +1,11 @@
 class Detail < ApplicationRecord
     belongs_to :item
     belongs_to :order
+
+    validates :quantitiy, presence: true,
+    numericality: {
+        only_integer: true,
+        allow_blank: true,
+        greater_than: 0
+    }
 end
