10e1d65c3b22342b19ebafe23ffdda4a6eb8a3a4
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-18 19:10:28 +0900

    カテゴリ名を日本語に変更・その他をついか

--- db/seeds/development/categories.rb
+++ db/seeds/development/categories.rb
@@ -1,4 +1,4 @@
-categorirs = %w(manuel comic CD/DVD software hardware goods)
+categorirs = %w(攻略本 攻略本以外の書籍 CD/DVD ゲームソフト ゲーム機本体 キャラクターグッズ その他)
 categorirs.each do |c|
     Category.create(
         category_name: "#{c}"
