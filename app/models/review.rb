91fc8de5e2a418195c4929a6ed4fd08a5f12876e
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-21 03:09:10 +0900

    レビューのバリデーションを設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :score, presence: true, numericality: {
+    only_integer: true,
+    allow_blank: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :text, length: {maximum: 500}
 end
