0e3f7d02afb880d007cf3bb702e8c93a4138219d
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-20 16:54:10 +0900

    商品情報を更新する機能を実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,5 +10,15 @@ class Admin::ItemsController < Admin::Base
   def edit #商品情報編集画面
     @item = Item.find(params[:id])
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    @item.assign_attributes(params[:item])
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "商品情報を更新しました"
+    else
+      render "edit"
+    end
+  end
+
 end
