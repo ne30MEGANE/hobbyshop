c20899de3ee4d7ac36a935e6fbe9e9679f532f09
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-21 15:32:57 +0900

    カート内の商品の合計金額を表示する機能を実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,7 @@ class CartsController < ApplicationController
     before_action :remove_errors, only: :add_item
 
     def show
-        @details = current_cart.details.order("item_id")
+        @details = current_cart.details.order("item_id") 
     end
     
     def add_item
