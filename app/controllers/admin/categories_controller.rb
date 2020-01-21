b74e09ef3732241f12d9082e397d43afc4afc5e5
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-21 15:11:15 +0900

    カテゴリの追加・名前変更機能を実装

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,4 +8,31 @@ class Admin::CategoriesController < Admin::Base
     @category.destroy
     redirect_to admin_categories_path, notice: "カテゴリを削除しました"
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to admin_categories_path, notice: "カテゴリを登録しました"
+    else
+      render new_admin_category_path
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.assign_attributes(params[:category])
+    if @category.save
+      redirect_to admin_categories_path, notice: "カテゴリ名を変更しました"
+    else
+      render edit_admin_category_path
+    end
+  end
 end
