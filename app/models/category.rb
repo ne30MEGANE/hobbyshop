851ed6d0e173619c8584ce3fb6e3528a19e4a325
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-27 21:26:25 +0900

    カテゴリ周り修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     has_many :items
 
-    validates :category_name, uniqueness: true, presence: true,length: {minimum: 2, maximum: 20}
+    validates :category_name, uniqueness: true, presence: true,length: {minimum: 2, maximum: 20, allow_blank: true}
 end
