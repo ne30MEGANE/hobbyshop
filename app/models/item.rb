086ba5fc2b1b8eb8d4725229ee58bdce24fa08be
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-29 20:58:04 +0900

    商品画像を登録する機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,28 +1,45 @@
 class Item < ApplicationRecord
-    belongs_to :category, optional: true
-    has_many :reviews
+  belongs_to :category, optional: true
+  has_many :reviews
+  has_one_attached :item_image
+  attribute :new_item_image
 
-    class<<self
-      def search(query)
-        rel = order("id")
-        if query.present?
-          rel = rel.where("item_name LIKE ? OR item_details LIKE ?", "%#{query}%", "%#{query}%")
-        end
-        rel
+  class<<self
+    def search(query)
+      rel = order("id")
+      if query.present?
+        rel = rel.where("item_name LIKE ? OR item_details LIKE ?", "%#{query}%", "%#{query}%")
       end
+      rel
     end
+  end
 
-    validates :item_name, presence: true
-    validates :stock, presence: true, 
-      numericality: {
+  validates :item_name, presence: true
+  validates :stock, presence: true, 
+    numericality: {
+      only_integer: true,
+      allow_blank: true,
+      greater_than_or_equal_to: 0
+    }
+  validates :price, presence: true,
+    numericality: {
         only_integer: true,
         allow_blank: true,
-        greater_than_or_equal_to: 0
-      }
-    validates :price, presence: true,
-      numericality: {
-          only_integer: true,
-          allow_blank: true,
-          greater_than: 0
-      }
+        greater_than: 0
+    }
+  validates if: :new_item_image do
+    if new_item_image.respond_to?(:content_type)
+      unless new_item_image.content_type.in?(ALLOWED_CONTENT_TYPES)
+        errors.add(:new_item_image, :invalid_image_type)
+      end
+    else
+      errors.add(:new_item_image, :invalid)
+    end
+  end
+  
+  before_save do
+    if new_item_image
+      self.item_image = new_item_image
+    end
+  end
 end
