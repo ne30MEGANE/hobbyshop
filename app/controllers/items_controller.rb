fa5134abd0cc5957e1140b9d411bb52fc4c462e7
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-29 22:13:52 +0900

    ページネーション設定し忘れを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,12 @@ class ItemsController < ApplicationController
   end
 
   def search
-    @items = Item.search(params[:q])
+    @items = Item.search(params[:q]).page(params[:page]).per(30)
     render "index"
   end
 
   def select
-    @items = Item.where(category_id: params[:c])
+    @items = Item.where(category_id: params[:c]).page(params[:page]).per(30)
     render "index"
   end
 end
