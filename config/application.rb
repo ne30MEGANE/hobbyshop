327f47fb6a345ee610416f8fb2438daefb99efbe
Takahashi Nonoka <megane.nt5mf@gmail.com>
2020-01-30 13:25:33 +0900

    エラーページ作成

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Hobbyshop
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
     config.action_controller.permit_all_parameters = true
+
+    config.exceptions_app = ->(env) do
+      ErrorsController.action(:show).call(env)
+    end
   end
 end
